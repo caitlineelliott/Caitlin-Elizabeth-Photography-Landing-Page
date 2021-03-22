5d22d377eb1f30c0a6cdb45017a40bb57466688f
Caitlin Elliott <ceelliott90@gmail.com>
2021-03-22 10:54:56 -0400

    built dynamic nav in js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
 // variables for main page & tablet / desktop nav
 const pageSections = document.querySelectorAll('.nav-element');
 const navContainer = document.querySelector('.nav-list');
+
+// main objectives
+
+/**
+ * @description builds dynamic nav on page load
+ * @param {event} event - page load event
+ */
+
+window.addEventListener('load', (event) => {
+    for (let i = 0; i < pageSections.length; i++) {
+        const newLi = document.createElement('li');
+        const newLiText = document.createElement('a');
+        newLiText.id = pageSections[i].id.slice(0, -8);
+        newLiText.innerText = newLiText.id;
+        newLi.classList.add('nav-links');
+        newLi.classList.add('nav-links');
+        newLiText.classList.add('nav-items');
+        navContainer.append(newLi);
+        newLi.append(newLiText);
+    }
+});

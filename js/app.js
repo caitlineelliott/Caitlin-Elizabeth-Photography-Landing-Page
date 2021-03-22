fc172f5ca1bbe9c1adefc9d47bfe0e9d3f355dff
Caitlin Elliott <ceelliott90@gmail.com>
2021-03-22 10:56:37 -0400

    added mobile nav active/resting states

--- js/app.js
+++ js/app.js
@@ -72,3 +72,25 @@ window.addEventListener('scroll', () => {
 // variables for mobile nav
 const hamIcon = document.querySelector('.hamburger-icon');
 const closeBtn = document.querySelector('.close-btn');
+
+/**
+ * @description toggles mobile nav active & resting states
+ */
+
+const restingNav = () => {
+    hamIcon.classList.remove('hamburger-none');
+    document.querySelector('nav').classList.add('nav');
+    document.querySelector('nav').classList.remove('mobile-nav');
+    closeBtn.classList.remove('close-btn-active');
+};
+
+const activeNav = () => {
+    hamIcon.classList.add('hamburger-none');
+    document.querySelector('nav').classList.add('mobile-nav');
+    document.querySelector('nav').classList.remove('nav');
+    closeBtn.classList.add('close-btn-active');
+    closeBtn.addEventListener('click', restingNav);
+    document.querySelector('.nav-list').addEventListener('click', restingNav);
+};
+
+hamIcon.addEventListener('click', activeNav);

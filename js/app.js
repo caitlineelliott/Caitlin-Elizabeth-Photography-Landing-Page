3f11034c33872b49afde24fc02c16f858f73eb15
Caitlin Elliott <ceelliott90@gmail.com>
2021-03-22 10:55:57 -0400

    added nav bg on scroll function

--- js/app.js
+++ js/app.js
@@ -57,3 +57,14 @@ navContainer.addEventListener('click', (e) => {
     let element = document.querySelector(`#${e.target.id}-section`);
     scrollTo(element);
 });
+
+/**
+ * @description applies nav background on scroll
+ */
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        document.querySelector('.nav').classList.add('nav-scrolled');
+    } else if (window.scrollY === 0) {
+        document.querySelector('.nav').classList.remove('nav-scrolled');
+    }
+});
